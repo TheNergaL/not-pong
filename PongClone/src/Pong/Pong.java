7155782789b788f423e1bf038450abd86b596fc3
thenergal <tuckerpo@buffalo.edu>
2017-08-03 11:13:18 -0400

    add background buffer to remove Swing flickering, add AI player

--- PongClone/src/Pong/Pong.java
+++ PongClone/src/Pong/Pong.java
@@ -3,6 +3,7 @@ package Pong;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -12,29 +13,45 @@ public class Pong extends Applet implements Runnable, KeyListener {
 	 */
 	Thread thread;
 	final int WIDTH = 700, HEIGHT = 500;
-	
 	HumanPaddle p1;
-	
+	AIPaddle p2;
 	Ball b1;
+	boolean begun;
+	Graphics gfx;
+	Image img;
 
 	public void init() {
+		begun = false;
+		img = createImage(WIDTH, HEIGHT);
+		gfx = img.getGraphics();
 		this.resize(WIDTH, HEIGHT);
 		thread = new Thread(this);
 		p1 = new HumanPaddle(1);
 		this.addKeyListener(this);
 		b1 = new Ball();
+		p2 = new AIPaddle(2, b1);
 		thread.start();
-		
+
 	}
 
+	@Override
 	public void paint(Graphics g) {
-		g.setColor(Color.black);
-		g.fillRect(0, 0, WIDTH, HEIGHT);
+		gfx.setColor(Color.black);
+		gfx.fillRect(0, 0, WIDTH, HEIGHT);
 		if (b1.getX() < -10 || b1.getX() > 710) {
 			g.drawString("Game Over", 350, 250);
+		} else {
+			p1.draw(gfx);
+			b1.draw(gfx);
+			p2.draw(gfx);
 		}
-		p1.draw(g);
-		b1.draw(g);
+
+		if (!begun) {
+			gfx.setColor(Color.white);
+			gfx.drawString("Totally Not Pong!", 340, 100);
+			gfx.drawString("Press Enter to Play", 340, 130);
+		}
+	g.drawImage(img, 0, 0, this);
 	}
 
 	public void update(Graphics g) {
@@ -43,13 +60,13 @@ public class Pong extends Applet implements Runnable, KeyListener {
 
 	public void run() {
 		while (true) {
-
-		
-
-			
-			repaint();
-			p1.move();
-			b1.move();
+			if (begun) {
+				repaint();
+				p1.move();
+				p2.move();
+				b1.move();
+				b1.checkCollision(p1, p2);
+			}
 			try {
 				Thread.sleep(10);
 			} catch (InterruptedException e) {
@@ -67,6 +84,8 @@ public class Pong extends Applet implements Runnable, KeyListener {
 			p1.setUp(true);
 		} else if (arg0.getKeyCode() == KeyEvent.VK_DOWN) {
 			p1.setDown(true);
+		} else if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+			begun = true;
 		}
 	}
 
